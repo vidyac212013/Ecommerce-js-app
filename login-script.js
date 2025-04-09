f62345e8c7bbff4456854c65a4ec4e094d4bb528
vidyac212013 <vidyackrishnan@gmail.com>
2025-04-09 17:42:18 +0530

    login script updated

---
+++ login-script.js
@@ -0,0 +1,14 @@
+function buttonAction(value,event){
+    window.location.href=`${value}.html`;
+
+    
+    // let navBtn=document.querySelectorAll('.nav-btn button');
+    // navBtn.forEach((ele)=>{
+    //     ele.getAttribute('class') && ele.setAttribute('class','')
+    // })
+    // console.log(event.currentTarget.className='active');
+}
+// if(window.location.pathname ==="/login.html"){
+//     document.getElementById("btn-login").setAttribute('class',"active")
+    
+//     }
